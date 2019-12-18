2e7051ca0154a9533d19191cf60fa6d421f5e19d
Tom <desktop-vm-ubuntu>
2019-12-18 16:40:48 +0300

    thymeleaf

--- src/main/java/springframework/guru/spring5webapp/model/Book.java
+++ src/main/java/springframework/guru/spring5webapp/model/Book.java
@@ -25,6 +25,8 @@ public class Book {
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name =" book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authorSet = new HashSet<>();
 
+    public Book() {}
+
     public Book(String title, String isbn, Publisher publisher, Set<Author> authorSet) {
         this.title = title;
         this.isbn = isbn;
